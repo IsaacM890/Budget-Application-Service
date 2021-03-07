98fc614a32b7e71b3bdfe4feb8a211acf378b4aa
IsaacM <Itshak890@gmail.com>
2021-03-07 20:23:20 +0200

    creating transaction model

---
+++ models/Transaction.js
@@ -0,0 +1,33 @@
+/** @format */
+
+const mongoose = require('mongoose');
+
+const TransactionSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  transactionType: {
+    type: String,
+  },
+  date: {
+    type: Date,
+  },
+  currency: {
+    type: String,
+  },
+  category: {
+    type: String,
+  },
+  amount: {
+    from: { type: Number, required: true },
+    to: { type: Number },
+  },
+  location: {
+    city: { type: String },
+    country: { type: String },
+  },
+  company: { type: String },
+});
+
+module.exports = Transaction = mongoose.model('transaction', TransactionSchema);
