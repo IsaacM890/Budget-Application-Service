63a6a4ba8868d75175ee4d224a8ef2117d291137
IsaacM <Itshak890@gmail.com>
2021-03-07 18:58:29 +0200

    edit config files + connect to db

--- config/db.js
+++ config/db.js
@@ -1,6 +1,20 @@
-const { config } = require('dotenv/types');
+/** @format */
+
 const mongoose = require('mongoose');
-const config = require('config');
+require('dotenv').config();
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGOURI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log('MongoDB Connected...!');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
 
-
+module.exports = connectDB;
